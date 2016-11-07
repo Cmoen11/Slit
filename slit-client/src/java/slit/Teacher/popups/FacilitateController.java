b7d40ba535ea63961cb1ab012c6b8a9c5522fcf1
Christian Moen <cmoen11@gmail.com>
2016-11-07 22:22:33 +0100

    ..

--- slit-client/src/java/slit/Teacher/popups/FacilitateController.java
+++ slit-client/src/java/slit/Teacher/popups/FacilitateController.java
@@ -111,7 +111,7 @@ public class FacilitateController {
 
     private ModuleRemote lookupModuleBeanRemote() {
         try {
-            Context c = new InitialContext();
+            Context c = new InitialContext();   
             return (ModuleRemote) c.lookup("java:comp/env/ModuleBean");
         } catch (NamingException ne) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
