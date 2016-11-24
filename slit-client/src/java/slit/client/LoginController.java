5c144b44dd5959559605e949839ce4ba7bbe4b21
Christian Moen <cmoen11@gmail.com>
2016-11-24 14:52:35 +0100

    wokring on course select even more.

--- slit-client/src/java/slit/client/LoginController.java
+++ slit-client/src/java/slit/client/LoginController.java
@@ -32,7 +32,6 @@ public class LoginController {
 
     @FXML TextField username;
     @FXML TextField password;
-    @FXML ComboBox courses_combo;
     @FXML JFXCheckBox rememberMe;
     ArrayList<CourseInfo> courses;
     ArrayList<String> courseNames;
@@ -49,74 +48,19 @@ public class LoginController {
             saveUsernameAndPassword(username.getText(), password.getText());
         else pref.putBoolean("rememberMe", false);
         
-        if (lookupLoginAuth_beanRemote()
-                .authAdminAccount(username.getText(),
-                        password.getText())) {
-            
-            // the user is an admin, check whenever the user wants to log into
-            // the admin panel or not.
-            Alert alert = new Alert(AlertType.CONFIRMATION);
-            alert.setTitle("Admin");
-            alert.setHeaderText("Administrator");
-            alert.setContentText("Vi ser at du er en administrator, "
-                    + "ønsker du å logge inn på administrasjonspanelet?");
-            Optional<ButtonType> result = alert.showAndWait();
-            
-            // handle the user input
-            if (result.get() == ButtonType.OK) {
-                // user selected yes. -> send user to admin panel
-                new MainAdmin().runGUI(Main.primaryStage);
-            } else {
-                // user selected no. -> send user to normal Login
-                normalLogin();
-            }
-        } else {
-            // user is not an admin. send user to normal inlogging.
-            normalLogin();
-        }
-    }
-
-    private void normalLogin() {
-        UserDetails user;
-        
-        try {
-            // get selecteted course
-            CourseInfo selectedCourse = courses.get(
-                    courses_combo.getSelectionModel().getSelectedIndex());
-            
-            // lookup user and retrive a userObject if username and password is
-            // correct, and null if not.
-            user = lookupLoginAuth_beanRemote().authUser(
-                    username.getText(), password.getText(),
-                    selectedCourse.getCourseID());
-            
-        } catch (Exception e) {
-            user = null; // if an error occurred, return null.
-        }
-        
-        // now check if the sign in were succsessful. 
+        UserDetails user = lookupLoginAuth_beanRemote().
+                authUser(username.getText(), 
+                password.getText());
         if (user != null) {
-            // username & password is correct, and user is in selected course.
-            
-            // set the Authorisation user
-            account.Authorisation.setUserData(user);
-            
-            if (user.getEmail().equals("@@")) {
-                firstTimeLoggedIn(user);
+            try {
                 
-            }else if (user.isTeacher()) {
-                // the user is a teacher of the selected course.
-                new TeacherMain().runGUI(Main.primaryStage, user);
-            } else {
-                //user is a student of the selected course.
-                //!! TODO, add the student GUI method call here.
-                new MainStudent().runGUI(Main.primaryStage, user);
+                new CourseSelectorController().runGUI(user);
+                Main.primaryStage.close();
+            }catch(Exception e) {
+                System.out.println("Fant ikke GUI fil");
+                e.printStackTrace();
             }
-
-        } else {
-            
-            // username, password or course do not match the selected user.
-            
+        } else { // username & passoword is wrong, or failed to connect to server
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Brukerfeil");
             alert.setContentText("Du har enten problemer med brukernavn, "
@@ -126,6 +70,87 @@ public class LoginController {
             alert.setHeaderText("Innlogging feilet");
             alert.showAndWait();
         }
+
+        
+        
+        // old code
+//        if (lookupLoginAuth_beanRemote()
+//                .authAdminAccount(username.getText(),
+//                        password.getText())) {
+//            
+//            // the user is an admin, check whenever the user wants to log into
+//            // the admin panel or not.
+//            Alert alert = new Alert(AlertType.CONFIRMATION);
+//            alert.setTitle("Admin");
+//            alert.setHeaderText("Administrator");
+//            alert.setContentText("Vi ser at du er en administrator, "
+//                    + "ønsker du å logge inn på administrasjonspanelet?");
+//            Optional<ButtonType> result = alert.showAndWait();
+//            
+//            // handle the user input
+//            if (result.get() == ButtonType.OK) {
+//                // user selected yes. -> send user to admin panel
+//                new MainAdmin().runGUI(Main.primaryStage);
+//            } else {
+//                // user selected no. -> send user to normal Login
+//                normalLogin();
+//            }
+//        } else {
+//            // user is not an admin. send user to normal inlogging.
+//            normalLogin();
+//        }
+    }
+
+    private void normalLogin() {
+//        UserDetails user;
+//        
+//        try {
+//            // get selecteted course
+//            CourseInfo selectedCourse = courses.get(
+//                    courses_combo.getSelectionModel().getSelectedIndex());
+//            
+//            // lookup user and retrive a userObject if username and password is
+//            // correct, and null if not.
+//            user = lookupLoginAuth_beanRemote().authUser(
+//                    username.getText(), password.getText(),
+//                    selectedCourse.getCourseID());
+//            
+//        } catch (Exception e) {
+//            user = null; // if an error occurred, return null.
+//        }
+//        
+//        // now check if the sign in were succsessful. 
+//        if (user != null) {
+//            // username & password is correct, and user is in selected course.
+//            
+//            // set the Authorisation user
+//            account.Authorisation.setUserData(user);
+//            
+//            if (user.getEmail().equals("@@")) {
+//                firstTimeLoggedIn(user);
+//                
+//            }else if (user.isTeacher()) {
+//                // the user is a teacher of the selected course.
+//                new TeacherMain().runGUI(Main.primaryStage, user);
+//            } else {
+//                //user is a student of the selected course.
+//                //!! TODO, add the student GUI method call here.
+//                new MainStudent().runGUI(Main.primaryStage, user);
+//            }
+//
+//        } else {
+//            
+//            // username, password or course do not match the selected user.
+//            
+//            Alert alert = new Alert(AlertType.ERROR);
+//            alert.setTitle("Brukerfeil");
+//            alert.setContentText("Du har enten problemer med brukernavn, "
+//                    + "passord eller kurs. \n Ta kontakt "
+//                    + "med en Administrator ved feil");
+//            
+//            alert.setHeaderText("Innlogging feilet");
+//            alert.showAndWait();
+//        }
     }
     
     /**
@@ -160,23 +185,15 @@ public class LoginController {
         else {
             courses = lookupLoginAuth_beanRemote().getCourses();
         }
-        
-        // get all course s.
-        
-        courseNames = new ArrayList<>();
-        
-        // add all course names into an arrayList to display in ComboBox
-        courses.stream().forEach((course) -> {
-            courseNames.add(course.getCourseName());
-        });
-        
-        // set courseNames into the ComboBox.
-        courses_combo.setItems(FXCollections.observableArrayList(courseNames));
-        // select the first option.
-        courses_combo.getSelectionModel().select(0);
 
     }
-
+    
+    public void sortOutCoursesByUser() {
+        
+        
+    }
+    
+    
     /**
      * The bean call.
      * @return an object to call bean methods from.
