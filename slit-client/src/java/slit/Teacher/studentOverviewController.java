8b1c15bf165d852feb0e6107be15b1a84be9a46a
Tor Ole Lerbæk <tor.ole.lerbaek@gmail.com>
2016-12-07 13:47:35 +0100

    opprette gui start, og opprettet controller

---
+++ slit-client/src/java/slit/Teacher/studentOverviewController.java
@@ -0,0 +1,15 @@
+
+package slit.Teacher;
+
+/**
+ *
+ * @author TorOle
+ */
+public class studentOverviewController {
+    
+    
+    public void pressMe() {
+        System.out.println("hey tor ole!");
+    }
+    
+}
