881226de4738258b89e74f17a76609d22993d7c2
Tor Ole Lerbæk <tor.ole.lerbaek@gmail.com>
2016-12-07 15:12:47 +0100

    Lagt til kommentarer i koden

--- slit-bean/src/java/sessionBeans/studentOverviewBean.java
+++ slit-bean/src/java/sessionBeans/studentOverviewBean.java
@@ -15,6 +15,9 @@ public class studentOverviewBean implements studentOverviewRemote {
     @PersistenceContext()
     EntityManager em;
     
+    // Overkjører metoden clickMe i studentOverveiwRemote klassen
+    // med samme navn. Har i oppgave å finne student hvor primærnøkkel
+    // er 1. Når denne / hvis denne finnes, returner brukernavn. 
     @Override
     public String clickMe() {
         Users user = em.find(Users.class, 1);
