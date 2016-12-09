40f19445940c0bc16e0e09d2e6e4347a345df4e2
Tor Ole Lerbæk <tor.ole.lerbaek@gmail.com>
2016-12-09 13:36:07 +0100

    fikset mer på studentOversikten

--- slit-client/src/java/slit/Teacher/studentOverviewController.java
+++ slit-client/src/java/slit/Teacher/studentOverviewController.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -13,7 +15,10 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import modul.ModuleDetails;
+import modul.ModuleSubmissionDetails;
+import modul.SubmissionBeanRemote;
 import sessionBeans.studentOverviewRemote;
+import transferClasses.StudentSubmissionHistory;
 import user_details.UserBeanRemote;
 
 /**
@@ -27,22 +32,66 @@ public class studentOverviewController {
     @FXML
     ListView<Label> studentList;
     
+    @FXML ListView<Label> moduleSubmissionListView;
     
     // Nedenfor angir vi at bønnene kan referes til med kortere navn, 
     // nærmere best overveiwBean og userOverveiw
     studentOverviewRemote overviewBean = lookupstudentOverviewBeanRemote();
     UserBeanRemote userOverveiw = lookupUserBeanRemote();
     ArrayList<UserDetails> students;
+    ArrayList<StudentSubmissionHistory> submissionsForSelectedUser;
+    
     public void initialize() {
         students = userOverveiw.getAllUsers();
-        
         for(UserDetails user : students) {
-            Label l = new Label(user.getUsername());
+            Label l = new Label(user.getUsername() +
+                    "(" + countModuleSubmissions(user.getId()) + ")");
             studentList.getItems().add(l);
                 
         }
+        if (!students.isEmpty()) {
+            studentList.getSelectionModel().select(0);
+            changeSubmissionList();
+        }
+        
+        studentList.getSelectionModel().selectedItemProperty()
+                .addListener((ObservableValue<? extends Label> observable, 
+                        Label oldValue, Label newValue) -> {
+            changeSubmissionList();
+        });
+        
+        
     }
     
+    private int countModuleSubmissions(int userID) {
+        ArrayList<StudentSubmissionHistory> temp;
+        temp = lookupSubmissionBeanRemote()
+                .getSubmissionHistoryFromUser(userID,
+                        Controller.getUser().getCourseID())
+                .getHistory();
+        
+        return temp.size();
+    }
+    
+    private void changeSubmissionList() {
+      
+        moduleSubmissionListView.getItems().clear();
+        
+        int index = studentList.getSelectionModel().getSelectedIndex();
+        UserDetails user = students.get(index);
+        
+        
+        
+        submissionsForSelectedUser = lookupSubmissionBeanRemote()
+                .getSubmissionHistoryFromUser(user.getId(),
+                        Controller.getUser().getCourseID())
+                .getHistory();
+        
+        for (StudentSubmissionHistory sh : submissionsForSelectedUser) {
+            Label l = new Label(sh.getModuleName());
+            moduleSubmissionListView.getItems().add(l);
+        }
+    }
     
     // Midlertidig testknapp forå se at at bean / interface fungerer som det skal
     public void pressMe() {
@@ -76,6 +125,16 @@ public class studentOverviewController {
         try {
             Context c = new InitialContext();
             return (UserBeanRemote) c.lookup("java:comp/env/UserBean");
+        } catch (NamingException ne) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
+            throw new RuntimeException(ne);
+        }
+    }
+
+    private SubmissionBeanRemote lookupSubmissionBeanRemote() {
+        try {
+            Context c = new InitialContext();
+            return (SubmissionBeanRemote) c.lookup("java:comp/env/SubmissionBean");
         } catch (NamingException ne) {
             Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", ne);
             throw new RuntimeException(ne);
